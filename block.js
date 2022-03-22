0146037674e20e29234d8a24c9227c9c7838230f
anetbor <anetborin@gmail.com>
2022-03-22 09:41:07 +0000

    Block2 anlegen

--- block.js
+++ block.js
@@ -12,5 +12,18 @@ class Block{
         Hash:        ${this.hash.substring(0,10)}
         Data         ${this.data}`; 
      }
+
+     static genesis(){
+         return new this("Genesis time", "-------", "abcd1234",[])
+     }
+
+     static mineBlock(lastBlock,blockData){
+        const timestamp = Date.now(); // Zeit seit dem 01.01.1970
+        const lastHash = lastBlock.hash;
+        const hash = "to do hash";
+        const data = blockData;
+
+    return new this(timestamp,lastHash,hash,data);
+     }
 }
 module.exports = Block;
